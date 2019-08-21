ee3aecf137596688a536d568737c4adb1f7ff96e
paw <pawsen@gmail.com>
2019-08-20 20:35:41 -0600

    Delete non-modular functions

--- benchmark4_reg/test2.py
+++ benchmark4_reg/test2.py
@@ -14,14 +14,31 @@ from pyvib.nonlinear_elements import Nonlinear_Element, Pnl, Tanhdryfriction
 from pyvib.signal import Signal
 from pyvib.subspace import Subspace
 
+"""Identify coefficient of tanh nonlinearity
+fnl = k*tanh(ẏ/eps)
+where eps is the regularization parameter specified a priory
+
+Depending on the value of eps, we get a good or bad identification. If we look
+at the BLA plot, we see that the noise level is high for the values of eps that
+result in poor estimation. For 'good' eps the noise floor is zero.
+
+So somehow certain values of eps result in data that seems to be disturbed by
+noise, which I think is a sign of unsteady data.
+The data is generated as noise-free
+
+fnsi good:
+eps = 0.1, 0.0001
+
+fnsi bad:
+eps = 0.01
+"""
+
 # data containers
 Data = namedtuple('Data', ['sig', 'uest', 'yest', 'uval', 'yval', 'utest',
                            'ytest', 'um', 'ym', 'covY', 'freq', 'lines',
                            'npp', 'Ntr'])
 Result = namedtuple('Result', ['est_err', 'val_err', 'test_err', 'noise',
                                'errvec', 'descrip'])
-nmax = 100
-info = 1
 p = 2
 weight = False
 add_noise = False
@@ -184,8 +201,8 @@ def identify_linear(data, n, r, subscan=True, info=2):
         lin_errvec = linmodel.extract_model(data.yval, data.uval)
         print(f"Best subspace model, n, r: {linmodel.n}, {linmodel.r}")
 
-        linmodel.estimate(n=n, r=r, weight=weight)
-        linmodel.optimize(weight=weight, info=info)
+        #linmodel.estimate(n=n, r=r, weight=weight)
+        #linmodel.optimize(weight=weight, info=info)
     else:
         linmodel.estimate(n=n, r=r, weight=weight)
         linmodel.optimize(weight=weight, info=info)
@@ -229,119 +246,6 @@ def evaluate_models(data, models, errvec, info=2):
         print(f'rms error val (db): \n{db(rms(val_err[:,0]))}')
         # print(f'rms error test: \n{rms(test_err)}  \ndb: \n{db(rms(test_err))}')
     return Result(est_err, val_err, test_err, noise, errvec, descrip)
-
-
-def identify(data, nlx, nly, nmax=25, info=2, fnsi=False):
-    # transient: Add one period before the start of each realization. Note that
-    # this is for the signal averaged over periods
-    Rest = data.yest.shape[2]
-    T1 = np.r_[data.npp*data.Ntr, np.r_[0:(Rest-1)*data.npp+1:data.npp]]
-
-    linmodel = Subspace(data.sig)
-    linmodel._cost_normalize = 1
-    linmodel.estimate(2, 5, weight=weight)
-    linmodel.optimize(weight=weight, info=info)
-
-    # estimate NLSS
-    model = NLSS(linmodel)
-    # model._cost_normalize = 1
-    model.add_nl(nlx=nlx, nly=nly)
-    model.set_signal(data.sig)
-    model.transient(T1)
-    model.optimize(lamb=100, weight=weight, nmax=nmax, info=info)
-    # get best model on validation data. Change Transient settings, as there is
-    # only one realization
-    nl_errvec = model.extract_model(data.yval, data.uval, T1=data.npp*data.Ntr,
-                                    info=info)
-    models = [linmodel, model]
-    descrip = [type(mod).__name__ for mod in models]
-
-    if fnsi:
-        # FNSI can only use 1 realization
-        R = 1
-        T1 = np.r_[data.npp*data.Ntr, np.r_[0:(R-1)*data.npp+1:data.npp]]
-        sig = deepcopy(data.sig)
-        # This is stupid, but unfortunately nessecary
-        sig.y = sig.y[:, :, 0][:, :, None]
-        sig.u = sig.u[:, :, 0][:, :, None]
-        sig.R = 1
-        sig.average()
-        fnsi1 = FNSI()
-        fnsi1.set_signal(sig)
-        fnsi1.add_nl(nlx=nlx)
-        fnsi1.estimate(n=2, r=5, weight=weight)
-        fnsi1.transient(T1)
-        fnsi2 = deepcopy(fnsi1)
-        fnsi2.optimize(lamb=100, weight=weight, nmax=nmax, info=info)
-        models = models + [fnsi1, fnsi2]
-        descrip = descrip + ['FNSI', 'FNSI optimized']
-
-    descrip = tuple(descrip)  # convert to tuple for legend concatenation
-    # simulation error
-    val = np.empty((*data.yval.shape, len(models)))
-    est = np.empty((*data.ym.shape, len(models)))
-    test = np.empty((*data.ytest.shape, len(models)))
-    for i, model in enumerate(models):
-        test[..., i] = model.simulate(data.utest, T1=data.npp*data.Ntr)[1]
-        val[..., i] = model.simulate(data.uval, T1=data.npp*data.Ntr)[1]
-        est[..., i] = model.simulate(data.um, T1=T1)[1]
-
-    Pest = data.yest.shape[3]
-    # convenience inline functions
-
-    def stack(ydata, ymodel): return \
-        np.concatenate(
-        (ydata[..., None], (ydata[..., None] - ymodel)), axis=2)
-
-    def rms(y): return np.sqrt(np.mean(y**2, axis=0))
-    est_err = stack(data.ym, est)  # (npp*R,p,nmodels)
-    val_err = stack(data.yval, val)
-    test_err = stack(data.ytest, test)
-    noise = np.abs(np.sqrt(Pest*data.covY.squeeze()))
-
-    if info:
-        print()
-        print(f"err for models: signal, {descrip}")
-        # print(f'rms error noise:\n{rms(noise)}     \ndb: \n{db(rms(noise))} ')
-        # only print error for p = 0. Almost equal to p = 1
-        print(f'rms error est (db): \n{db(rms(est_err[:,0]))}')
-        print(f'rms error val (db): \n{db(rms(val_err[:,0]))}')
-        # print(f'rms error test: \n{rms(test_err)}  \ndb: \n{db(rms(test_err))}')
-    return Result(est_err, val_err, test_err, noise, nl_errvec, descrip)
-
-
-def plot(res, data, p):
-    figs = {}
-    lines = data.lines
-    freq = data.freq
-    Pest = data.yest.shape[3]
-
-    # result on validation data
-    N = len(data.yval)
-    freq = np.arange(N)/N*fs
-    plottime = res.val_err
-    plotfreq = np.fft.fft(plottime, axis=0)/np.sqrt(N)
-    plt.figure()
-    plt.plot(freq[lines], db(plotfreq[lines, p]), '.')
-    plt.plot(freq[lines], db(np.sqrt(Pest*data.covY[lines, p, p].squeeze() / N)),
-             '.')
-    plt.xlabel('Frequency')
-    plt.ylabel('Output (errors) (dB)')
-    plt.legend(('Output',) + res.descrip + ('Noise',))
-    plt.title(f'Validation results p:{p}')
-    figs['val_data'] = (plt.gcf(), plt.gca())
-
-    # optimization path for NLSS
-    plt.figure()
-    plt.plot(db(res.errvec))
-    imin = np.argmin(res.errvec)
-    plt.scatter(imin, db(res.errvec[imin]))
-    plt.xlabel('Successful iteration number')
-    plt.ylabel('Validation error [dB]')
-    plt.title('Selection of the best model on a separate data set')
-    figs['pnlss_path'] = (plt.gcf(), plt.gca())
-
-    return figs
 
 
 def plot_val(res, data, p):
@@ -439,19 +343,21 @@ def savefig(fname, figs):
             f[0].savefig(f"{fname}{k}{i}.png")
 
 
-def ident(data, nlx, nly, n, r, subscan=True):
+def identify(data, nlx, nly, n, r, subscan=True):
     errvec = {}
     models = {}
+    models['lin'], _ = identify_linear(
+        data, n=n, r=r, subscan=subscan, info=info)
     models['fnsi'], _ = identify_fnsi(
         data, nlx, nly, n=n, r=r, nmax=nmax, optimize=False, info=info)
     models['fnsi optim'], errvec['fnsi'] = identify_fnsi(
         data, nlx, nly, n=n, r=r, nmax=nmax, optimize=True, info=info)
-    models['lin'], _ = identify_linear(
-        data, n=n, r=r, subscan=subscan, info=info)
     models['nlss'], errvec['nlss'] = identify_nlss(
         data, models['lin'], nlx, nly, nmax=nmax, info=info)
-    # models['nlss_pnl'], errvec['nlss_pnl'] = identify_nlss(
-    #     data, models['lin'], nlx_pnl, nly, nmax=nmax, info=info)
+
+    nlx_pnl = [Pnl(degree=[2, 3, 5], structure='statesonly')]
+    models['nlss_pnl'], errvec['nlss_pnl'] = identify_nlss(
+        data, models['lin'], nlx_pnl, nly, nmax=nmax, info=info)
     res = evaluate_models(data, models, errvec, info=info)
     return models, res
 
@@ -464,10 +370,13 @@ def disp_plot(data, res, nldof):
     figs = {**f1, **f2, **f3, **f4}
     return figs
 
-
+# parameters
+nmax = 100
+info = 1
+subscan = False
 nldof = 1
 
-tahn1 = Tanhdryfriction(eps=0.1, w=[0, 1])
+tahn1 = Tanhdryfriction(eps=0.01, w=[0, 1])
 nlx = [tahn1]
 F = np.array([])
 nly = None
@@ -480,11 +389,12 @@ true_model.add_nl(nlx=nlx, nly=nly)
 raw_data3 = simulate(true_model, npp=2048, Ntr=2)
 #raw_data3 = np.load('data/test.npz')
 
-data3 = partion_data(raw_data3)
-res31 = identify(data3, nlx, nly, nmax=nmax, info=info, fnsi=True)
+# Ntr: how many transient periods in T1 for identification
+data3 = partion_data(raw_data3, Ntr=1)
+# plot bla to see nonlinear distortion. Check noise level!
+plot_bla([], data3, nldof)
 
-subscan = False
-models, res3 = ident(data3, nlx, nly, n=2, r=5, subscan=subscan)
+models, res3 = identify(data3, nlx, nly, n=2, r=5, subscan=subscan)
 figs = disp_plot(data3, res3, nldof)
 
 # subspace plots
@@ -495,6 +405,4 @@ if subscan:
 # plot periodicity for one realization to verify data is steady state
 figs['per'] = data3.sig.periodicity(dof=nldof)
 
-
-figs1 = plot(res31, data3, p=1)
-figs2 = plot_bla(res31, data3, p=1)
+savefig('fig/test_', figs)
