131b4c35065a559ec3240efa77d568702edd658f
Nidish Narayanaa Balaji <nidbid@gmail.com>
2019-11-12 14:02:27 -0600

    Create odesys_ss.m

---
+++ benchmark5/odesys_ss.m
@@ -0,0 +1,29 @@
+function dxdt = odesys_ss(t,x, u, model)
+% system with NL.
+% fex should either be a function or interpolated table
+
+dxdt = model.A*x + model.B*u(t) + model.fnls*fnl(x(model.nx), x(model.nxd), model.nlpars);
+end
+
+function f = fnl(q,u,par)
+% calculate nonlinear force
+% q: displacement
+% u: velocity
+
+f = zeros(size(q,1),1);
+nonlinear_elements = par.nonlinear_elements;
+for nl=1:length(nonlinear_elements)
+    % Determine force direction and calculate displacement and velocity of
+    % nonlinear element
+    w = nonlinear_elements{nl}.force_direction;
+    qnl = w'*q; unl = w'*u;
+    
+%     switch lower(nonlinear_elements{nl}.type)
+%     case 'unilateralspring'
+    f = f + ...
+        w*nonlinear_elements{nl}.stiffness*...
+        (qnl-nonlinear_elements{nl}.gap).*...
+        double(qnl-nonlinear_elements{nl}.gap>=0);
+%     end
+end
+end
